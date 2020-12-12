eaff15acb6290bd4a3bc766a3dcc0dbd6e9f5ef9
EthanFrier <etfr1107@colorado.edu>
2020-12-12 16:49:49 -0500

    changed cube to rotate with arrow keys

---
+++ Assets/Scripts/Rotate_Keys.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Keys : MonoBehaviour
+{
+    public float rotateSpeed = 5f;
+    public bool grabbed;
+
+    void Start()
+    {
+        grabbed = false;
+    }
+
+    void Update()
+    {
+        float spinX = Input.GetAxis("Vertical") * rotateSpeed;
+        float spinY = Input.GetAxis("Horizontal") * rotateSpeed;
+
+        if (spinX != 0)
+        {
+            grabbed = true;
+            transform.Rotate(Vector3.right, spinX, Space.World);
+        }
+        else if (spinY != 0)
+        {
+            grabbed = true;
+            transform.Rotate(Vector3.up, -spinY, Space.World);
+        }
+        else if (spinX == 0 && spinY == 0)
+        {
+            grabbed = false;
+        }
+
+    }
+}
