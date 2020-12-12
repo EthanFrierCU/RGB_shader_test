650a2bb4f94351fe5cd5744a72b13e65a9693da7
EthanFrier <etfr1107@colorado.edu>
2020-12-12 16:16:29 -0500

    switched from building for VR (quest2) to desktop and eventually webgl

---
+++ Assets/Scripts/rotate_wmouse.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate_wmouse : MonoBehaviour
+{
+    public float rotateSpeed = 20f;
+    public bool grabbed;
+
+    void Start()
+    {
+        grabbed = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        grabbed = true;
+
+        float rotX = Input.GetAxis("Mouse X") * rotateSpeed;
+        float rotY = Input.GetAxis("Mouse Y") * rotateSpeed;
+
+        transform.Rotate(Vector3.up, -rotX);
+        transform.Rotate(Vector3.right, rotY);
+
+    }
+
+    private void OnMouseUp()
+    {
+        grabbed = false;
+    }
+
+
+    //void Update()
+    //{
+
+    //}
+}
