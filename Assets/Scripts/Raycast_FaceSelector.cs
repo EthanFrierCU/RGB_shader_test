f39317e0b76ac1ebbfb2ccfa5b4f80cddc043eb4
EthanFrier <etfr1107@colorado.edu>
2020-12-12 17:39:15 -0500

    selection faces change material on raycast hit

---
+++ Assets/Scripts/Raycast_FaceSelector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast_FaceSelector : MonoBehaviour
+{
+    public Material activeMaterial;
+    public Material defaultMaterial;
+    public string kind = "face";
+    private Transform _selection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(kind))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = activeMaterial;
+                }
+
+                _selection = selection;
+            }
+          
+        }
+    }
+}
