ec3e08816d666c3e5c9934bf015b449ad6d5b634
EthanFrier <etfr1107@colorado.edu>
2020-12-13 12:42:58 -0500

    face sliders working

--- Assets/Scripts/MouseDrag_x_axis.cs
+++ Assets/Scripts/MouseDrag_x_axis.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseDrag_y_axis : MonoBehaviour
+public class MouseDrag_x_axis : MonoBehaviour
 {
     public float dragSpeed = .001f;
-    public float minY = -2;
-    public float maxY = 2;
+    public float minX = -1;
+    public float maxX = 1;
     private Vector3 lastMousePos;
 
     void OnMouseDown()
@@ -18,10 +18,9 @@ public class MouseDrag_y_axis : MonoBehaviour
     {
         Vector3 delta = Input.mousePosition - lastMousePos;
         Vector3 pos = transform.localPosition;
-        pos.y += delta.y * dragSpeed;
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.x += delta.x * dragSpeed;
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
         transform.localPosition = pos;
         lastMousePos = Input.mousePosition;
     }
 }
-
--- Assets/Scripts/MouseDrag_z_axis.cs
+++ Assets/Scripts/MouseDrag_z_axis.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseDrag_y_axis : MonoBehaviour
+public class MouseDrag_z_axis : MonoBehaviour
 {
     public float dragSpeed = .001f;
-    public float minY = -2;
-    public float maxY = 2;
+    public float minZ = -1;
+    public float maxZ = 1;
     private Vector3 lastMousePos;
 
     void OnMouseDown()
@@ -18,10 +18,9 @@ public class MouseDrag_y_axis : MonoBehaviour
     {
         Vector3 delta = Input.mousePosition - lastMousePos;
         Vector3 pos = transform.localPosition;
-        pos.y += delta.y * dragSpeed;
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z += delta.x * dragSpeed;
+        pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
         transform.localPosition = pos;
         lastMousePos = Input.mousePosition;
     }
 }
-
