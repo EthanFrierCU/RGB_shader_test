dae49d35a5a909c4fae8c3c97e76c0d5de587bb5
EthanFrier <etfr1107@colorado.edu>
2020-12-14 10:35:47 -0500

    trying to solve rotation issues

--- Assets/Scripts/Rotate_Keys.cs
+++ Assets/Scripts/Rotate_Keys.cs
@@ -6,10 +6,12 @@ public class Rotate_Keys : MonoBehaviour
 {
     public float rotateSpeed = 5f;
     public bool grabbed;
+    private Vector3 currentEulerAngles;
 
     void Start()
     {
         grabbed = false;
+        
     }
 
     void Update()
@@ -20,17 +22,21 @@ public class Rotate_Keys : MonoBehaviour
         if (spinX != 0)
         {
             grabbed = true;
-            transform.Rotate(Vector3.right, spinX);
+            currentEulerAngles.x += spinX;
+            transform.eulerAngles = currentEulerAngles;
         }
         else if (spinY != 0)
         {
             grabbed = true;
-            transform.Rotate(Vector3.up, -spinY);
+            currentEulerAngles.y -= spinY;
+            transform.eulerAngles = currentEulerAngles;
         }
         else if (spinX == 0 && spinY == 0)
         {
             grabbed = false;
         }
+
+
 
     }
 }
